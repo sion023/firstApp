6bd3d7f1448b5600dd3470f44d107956516e37e4
sion023 <usachan440@gmail.com>
2019-05-23 13:33:39 +0900

    p&d

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -29,6 +30,8 @@ export class HomePage {
     createdDate: '5分前'
   }];
 
+  constructor(private alertCtrl: AlertController) {}
+
   addPost() {
     this.post = {
       userName: 'ebsloftt0536',
@@ -37,5 +40,39 @@ export class HomePage {
     };
     this.posts.push(this.post);
     this.message = '';
+  }
+
+  async presentPrompt(index: number) {
+    const alert = await this.alertCtrl.create({
+      header: 'メッセージ編集',
+      inputs: [
+        {
+          name: 'message',
+          type: 'text',
+          placeholder: 'メッセージ'
+        }
+      ],
+      buttons: [
+        {
+          text: 'キャンセル',
+          role: 'cancel',
+          handler: () => {
+            console.log('キャンセルが選択されました');
+          }
+        },
+        {
+          text: '更新',
+          handler: data => {
+            console.log(data);
+            this.posts[index].message = data.message;
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  deletePost(index: number) {
+    this.posts.splice(index, 1);
   }
 }
