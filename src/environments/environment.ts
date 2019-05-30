3b2f05e5bcfb8f9a2ddae014a257d2b14df99e07
sion023 <usachan440@gmail.com>
2019-05-30 13:57:09 +0900

    ,

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: 'AIzaSyC89zmFaEVz5mwEswBJwhE2tiyDxjr6zgw',
+    authDomain: 'ionic-boad-67f38.firebaseapp.com',
+    databaseURL: 'https://ionic-boad-67f38.firebaseio.com',
+    projectId: 'ionic-boad-67f38',
+    storageBucket: 'ionic-boad-67f38.appspot.com',
+    messagingSenderId: '885581896844',
+    appId: '1:885581896844:web:ff890d8bf3f0189b'
+  }
 };
 
 /*
